b4ddee0e618e61af9368d43e319c3104dfe1f10f
unknown <nkanaparthyper@live.com>
2020-02-13 00:56:07 -0700

    Removed stock card and included custom Post Card

---
+++ components/PostCard.vue
@@ -0,0 +1,72 @@
+<template>
+  <a class="card bm--card-equal-height" href="#">
+    <header class="card-header">
+        <div class="card-header-title has-text-grey" v-if="title">
+            {{title}}
+        </div>
+    </header>
+      
+    <div class="card-content">
+        <div class="content">
+            <b-icon v-if="feature_image"
+                :icon="feature_image"
+                size="is-medium"
+                type="is-primary"
+            />
+            <b-icon v-else
+                icon="github-circle"
+                size="is-medium"
+                type="is-primary"
+            />            
+            <div v-if="content" class="is-clipped">{{content}}</div>
+        </div>
+    </div>
+
+    <footer class="card-footer">
+        <a href="#" class="card-footer-item">{{read_time}} min</a>
+    </footer> 
+  </a>
+</template>
+
+<script>
+export default {
+    props:{
+        title:{
+            type: String,
+            required: true
+        },
+        content:{
+            type: String,
+            required: true
+        },
+        feature_image:{
+            type: String,
+            required: false
+        },
+        read_time:{
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+.bm--card-equal-height {
+   display: flex;
+   flex-direction: column;
+   height: 100%;
+}
+.bm--card-equal-height .card-footer {
+   margin-top: auto;
+}
+.card {
+  border-radius: 25px;
+}
+.card-footer{
+  border-radius: 0px 0px 25px 25px;
+}
+.card-header{
+  border-radius: 25px 25px 0px 0px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,32 @@
 <template>
   <section class="section">
-    <div class="columns is-mobile">
-      <card
-        title="Free"
-        icon="github-circle"
-      >
-        Open source on <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
-      </card>
-
-      <card
-        title="Responsive"
-        icon="cellphone-link"
-      >
-        <b class="has-text-grey">
-          Every
-        </b> component is responsive
-      </card>
-
-      <card
-        title="Modern"
-        icon="alert-decagram"
-      >
-        Built with <a href="https://vuejs.org/">
-          Vue.js
-        </a> and <a href="http://bulma.io/">
-          Bulma
-        </a>
-      </card>
-
-      <card
-        title="Lightweight"
-        icon="arrange-bring-to-front"
-      >
-        No other internal dependency
-      </card>
+    <div class="columns is-desktop">
+      <div v-for="post in posts" track-by="index" class="column is-4">
+        <h1 v-if="post.index!=-1">{{post.index}}</h1>
+        <PostCard
+          :title="post.title"
+          :content="post.excerpt"
+          :feature_image="post.feature_image"
+          :read_time="post.reading_time"
+        />
+      </div>
     </div>
   </section>
 </template>
 
 <script>
-import Card from '~/components/Card'
-
+import PostCard from "~/components/PostCard"
+import {getPosts} from "~/middleware/ghost"
 export default {
   name: 'HomePage',
 
   components: {
-    Card
+    PostCard
+  },
+  async asyncData(){
+    const posts = await getPosts();
+    console.info(posts);
+    return {posts: posts}
   }
 }
 </script>
